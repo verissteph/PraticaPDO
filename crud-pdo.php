313cad43341b27261160249565d9b764b9952a00
verissteph <stephanieverissimopp@gmail.com>
2020-02-21 16:30:47 -0300

    Verificando se tem erros na conexão, caso haja irá mostrar qual o erro

--- crud-pdo.php
+++ crud-pdo.php
@@ -1,4 +1,10 @@
-<?php 
-$pdo = new PDO("mysql:dbname=crudpdo;host:localhost","root"," ");
+<?php
+try{
+    $pdo = new PDO("mysql:dbname=crudpdo;host:localhost","root"," ");
 
+} catch(PDOException $e){
+    echo "Erro com o banco de dados:". $e ->getMessage();
+} catch (PDOException $e) {
+    echo "Erro generico:". $e ->getMessage();
+}
 ?>
