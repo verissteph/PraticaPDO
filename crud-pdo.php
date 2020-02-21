bb47335576cfeb766f170ee7d50ac5e159cabd44
verissteph <stephanieverissimopp@gmail.com>
2020-02-21 16:45:53 -0300

    Erro com a senha apoś o root, pois estava com espaço entre as aspas vazias. Problema resolvido

--- crud-pdo.php
+++ crud-pdo.php
@@ -1,10 +1,8 @@
 <?php
-try{
-    $pdo = new PDO("mysql:dbname=crudpdo;host:localhost","root"," ");
-
-} catch(PDOException $e){
-    echo "Erro com o banco de dados:". $e ->getMessage();
+try {
+    $pdo = new PDO("mysql:dbname=CRUDPDO;host:127.0.0.1", "root", "");
 } catch (PDOException $e) {
-    echo "Erro generico:". $e ->getMessage();
+    echo "Erro com o banco de dados:" . $e->getMessage();
+} catch (PDOException $e) {
+    echo "Erro generico:" . $e->getMessage();
 }
-?>
