9ac088659772713a036ad18ae98ee8eddb478558
verissteph <stephanieverissimopp@gmail.com>
2020-02-22 12:26:09 -0300

    Fazendo com que as informações aparecam de forma 'dinamica' utilizando loops de php dentro do html

--- CRUD-PESSOA/index.php
+++ CRUD-PESSOA/index.php
@@ -1,3 +1,11 @@
+<?php
+//Aqui iremos embutir o php com html e css
+//primeiro chamamos com require_onde o arquivo onde criamos a função
+require_once 'class-pessoa.php';
+//segundo vamos instanciar a class Pessoa por meio da criação de uma nova variável
+$p = new Pessoa("crudpdo", "localhost", "root", "");
+//terceiro vamos acessar o metodo que criamos, abrindo o php lá abaixo da section direito
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,18 +37,31 @@
                     <td>TELEFONE</td>
                     <td colspan="2">E-MAIL</td>
                 </tr>
-                <tr>
-                    <td>Maria</td>
-                    <td>0000000</td>
-                    <td>maria@gmail.com</td>
-                    <td>
-                        <a href="#">Editar</a>
-                        <a href="#">Excluir</a>
-                    </td>
-                </tr>
+                <?php
+                $dados = $p->buscarDados(); //atribuindo o array de dados a uma variavel. Array de array.
+                //var_dump($dados); estava verificando como a variavel $dados estava se comportava, pois estava dando erro, nada era preenchido. O erro é que estava usando prepare ao inves de query na function buscarDados.
+                $tamanho_dados = count($dados); //criando variavel para contar o tamanho do array de dados
+                if ($tamanho_dados > 0) { //se o array não estiver vazio
+                    for ($i = 0; $i < $tamanho_dados; $i++) { //executa o for para repetir o primeiro array que é a posição de cada pessoa
+                        echo "<tr>"; //abre antes do foreach por que o for vai criar linha na posição 0 e mostrar todos os dados da pessoa dessa posição e assim por diante até a ultima pessoa (lembre que o foreach vai dar loop nos dados de cada pessoa)
+                        foreach ($dados[$i] as $k => $v) { //executa o foreach para repetir as info do segundo array que tem os dados de cada pessoa
+                            if ($k != "id") { // a coluna id nao aparece na nossa tabela, logo só irá rodar caso a coluna em questão não seja a coluna que contem o id
+                                echo "<td>" . $v . "</td>"; //vai exibir as informações de nome,telefone e email 
+                            }
+                        }
+                        ?>
+                        <td>
+                            <a href="">Editar</a><a href="">Excluir</a>
+                        </td>
+                        <?php
+                        echo "</tr>";
+                        //faz o td dentro do FOR porque precisa repetir os botoes a cada linha repetida 
+                    }
+                }
+                ?>
             </table>
         </section>
-</div>
+    </div>
 </body>
 
 </html>
